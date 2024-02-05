9d3f5bb1a660f9e80b17db2a5e3eb52ec5f806a3
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-05 09:46:58 +0300

    adding header and players name

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe tic </h1>
-  )
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player-name">Player 1</span>{" "}
+          <span className="palyer-symbol">X</span>
+        </li>
+        <li>
+          <span className="player-name">Player 2</span>{" "}
+          <span className="palyer-symbol">O</span>
+        </li>
+      </ol>
+    </div>
+  );
 }
 
-export default App
-{/* <div id="game-container">
-        <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>{" "}
-            <span className="palyer-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>{" "}
-            <span className="palyer-symbol">O</span>
-          </li>
-        </ol> */}
-      // </div>
+export default App;

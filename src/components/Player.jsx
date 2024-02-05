ffb4f2d815c39b489a4a08d667b6a43b156ee963
bushrahurre959@gmail.com <marriamhurre@gmail.com>
2024-02-05 10:19:49 +0300

    working with state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const HandlerEditClick = () => {
+    setIsEditing(true);
+  };
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" name="" id="" />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>edit</button>
+      <button onClick={HandlerEditClick}>edit</button>
     </li>
   );
 }
+
+//another way of rendering content conditionally
+
+{
+  /* <li>
+      <span className="player">
+        {/* {isEditing && <span className="player-name">{name}</span>} */
+}
+//         {isEditing ? <span className="player-name">{name}</span> : null}
+//         <span className="player-symbol">{symbol}</span>
+//       </span>
+//       <button onClick={editHandler}>edit</button>
+//       {!isEditing && <input type="text" name="" id="" />}
+
+// </li> */}
